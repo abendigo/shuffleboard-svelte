f11cd1dd8444f17423ba9408bfb155312b784539
Mark Oosterveld <mark@oosterveld.org>
2022-04-16 18:02:30 -0400

    added Storybook

---
+++ src/stories/App.stories.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Index from "../routes/index.svelte";
+</script>
+
+<Meta
+  title="Index/Page"
+  component={Index}
+  />
+
+<Template let:args>
+  <Index />
+</Template>
+
+<Story
+  name="LoggedIn"
+  args={{
+    user: {},
+  }}
+/>
+
