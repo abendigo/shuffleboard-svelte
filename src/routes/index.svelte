4dd0881d45916a3ddb8b6e6ab6a5cc9cb63ec71d
Mark Oosterveld <mark@oosterveld.org>
2022-04-16 15:27:41 -0400

    we cant use paths right now

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
+<script>
+  // import { Greeter } from "$lib/greeter/index";
+  import { Greeter } from "../lib/greeter/index"
+
+  let xxx = new Greeter().sayHello();
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Marko was here: {xxx}</p>
